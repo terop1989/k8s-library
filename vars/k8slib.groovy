7b2250a3e5ebe39d0cf4a1f4d1ec0e54e669410b
Yuriy Kozhin <YKozhin@IBS.RU>
2023-09-29 12:45:24 +0300

    rename file in workspace

--- vars/k8slib.groovy
+++ vars/k8slib.groovy
@@ -25,12 +25,12 @@ def call(body) {
             sh "pwd"
             sh "ls -la"
 
-                writeFile file: 'k8s-agent.dockerfile', text:  (libraryResource 'k8s-agent.dockerfile')
+                writeFile file: 'k8s-agent.dockerfile.ws', text:  (libraryResource 'k8s-agent.dockerfile')
                 k8sAgentBuildName = 'k8s-agent:latest'
                 k8sAgentBuildArgs = ''
                 k8sAgentRunArgs = '-u 0:0'
             
-            def k8sAgent = docker.build("${k8sAgentBuildName}", "${k8sAgentBuildArgs} -f ${k8sAagentFile} .")
+            def k8sAgent = docker.build("${k8sAgentBuildName}", "${k8sAgentBuildArgs} -f k8s-agent.dockerfile.ws .")
 
             stage('Cleanup') {
                 deleteDir()
