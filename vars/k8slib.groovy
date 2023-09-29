1bbc2c947b3776b2396c05ac738248b170930bdb
Yuriy Kozhin <YKozhin@IBS.RU>
2023-09-29 12:56:21 +0300

    delete whitespaces

--- vars/k8slib.groovy
+++ vars/k8slib.groovy
@@ -25,12 +25,12 @@ def call(body) {
             sh "pwd"
             sh "ls -la"
 
-                def k8sAgentFile = libraryResource 'k8s-agent.dockerfile'
-                    k8sAgentFileName = 'k8s-agent.dockerfile.ws'
-                    writeFile file: k8sAgentFileName , text: k8sAgentFile
-                    k8sAgentBuildName = 'k8s-agent:latest'
-                    k8sAgentBuildArgs = ''
-                    k8sAgentRunArgs = '-u 0:0'
+            def k8sAgentFile = libraryResource 'k8s-agent.dockerfile'
+                k8sAgentFileName = 'k8s-agent.dockerfile.ws'
+                writeFile file: k8sAgentFileName , text: k8sAgentFile
+                k8sAgentBuildName = 'k8s-agent:latest'
+                k8sAgentBuildArgs = ''
+                k8sAgentRunArgs = '-u 0:0'
             
             def k8sAgent = docker.build("${k8sAgentBuildName}", "${k8sAgentBuildArgs} -f ${k8sAgentFileName} .")
 
