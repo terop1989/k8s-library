d2049ee3c9173dc6cf9fe28c345710de4bed3b00
Yuriy Kozhin <YKozhin@IBS.RU>
2023-09-29 10:51:17 +0300

    make dir for app

--- vars/k8slib.groovy
+++ vars/k8slib.groovy
@@ -11,33 +11,17 @@ def call(body) {
 
         node('master') {
 
-            stage('Checkout SCM') {
-                checkout scm
-                echo "Branch name is ${env.BRANCH_NAME}\nTag name is ${env.TAG_NAME}"
-            }
+            cleanWs()
 
-            if (env.TAG_NAME ==~ /\d+\.\d+\.\d+-release/) {
-                release_number = env.TAG_NAME.split('-')[0]
-                println ("Release Number = " + release_number)
+            sh "mkdir app"
 
-                DockerRepositoryAddress='docker.io'
-                stage('Docker Build') {
-                    withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
-                        sh """
-                        docker login ${DockerRepositoryAddress} -u $DOCKER_USER -p $DOCKER_PASSWORD
-                        docker build -t ${DOCKER_USER}/${pipelineParams.projectName}:${release_number} ./app/
-                        docker push     ${DOCKER_USER}/${pipelineParams.projectName}:${release_number}
-                        """
-                    }
+            dir("app") {
+
+                stage('Checkout SCM') {
+                    checkout scm
+                    echo "Branch name is ${env.BRANCH_NAME}\nTag name is ${env.TAG_NAME}"
                 }
 
-                jenkinsAgentDockerfilePath = "${env.WORKSPACE}" + "@libs/" + "${pipelineParams.ext_lib_name}"
-                jenkinsAgentDockerfileName = "${jenkinsAgentDockerfilePath}" + "/agent.dockerfile"
-                jenkinsAgentBuildName = 'agent:latest'
-                jenkinsAgentBuildArgs = ''
-                jenkinsAgentRunArgs = " -u 0:0 -v ${jenkinsAgentDockerfilePath}:/mnt"
-
-                def RunAgent = docker.build("${jenkinsAgentBuildName}", "${jenkinsAgentBuildArgs} -f ${jenkinsAgentDockerfileName} .")
             }
 
             stage('Cleanup') {
